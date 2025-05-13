58081b2ed2275ab7df4f1151068e6145f1f1dd31
Hadiseh2006 <eng.h.zamani85@gmail.com>
2025-05-13 22:36:21 +0330

    Create struct Book.cpp

---
+++ struct Book.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+// ساختار کتاب
+struct Book {
+    int id;                         // کد کتاب
+    string title;                   // عنوان کتاب
+    string author;                  //نویسنده کتاب
+    int year;                       // سال انتشار
+};
+struct Library{
+    int n;                          //تعداد کتابهای کتابخانه
+    Book *books;                     //ارایه ای از کتابها
+};
+void addbook()
+int main()
+{
+    Library lib;
+    lib.books=unllptr
+    lib.n=0
+    cout << "Hello world!" << endl;
+    return 0;
+}
